e8c213b339cf799318acc784b30ca9af79395f37
Pedro Gomes <pedromgomes29@gmail.com>
2023-03-25 18:52:06 +0000

    Final annealing

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import pandas as pd
 import ast
 import math
 from utils import init_variables,is_legal,evaluate_solution
-from simulated_annealing import get_sa_solution
+from simulated_annealing import get_sa_solution,get_sa_solution_adaptive_with_stages
 from hill_climbing import get_hc_solution
+from multiprocessing import Process
 
 distancesFileName = "distances.csv"
 establishmentsFileName = "establishments.csv"
@@ -16,7 +17,7 @@ distances = pd.read_csv(distancesFileName,index_col=0)
 establishments = pd.read_csv(establishmentsFileName, converters=conv)
 
 
-num_establishments = 200
+num_establishments = 100
 
 num_vehicles = math.floor(0.1*num_establishments)
 
@@ -25,16 +26,22 @@ END_OF_SHIFT = datetime.time(17, 0)
 init_variables(distances, establishments, num_establishments, num_vehicles, END_OF_SHIFT)
 
 init_time = datetime.datetime.now()
-"""
-solution = simulated_annealing(0.001,0.1,1000)
-print(is_legal(solution))
-print(solution)
-print(evaluate_solution(solution))
+
+def normal_sa():
+    solution = get_sa_solution(5000)
+    print(f"Normal SA: {evaluate_solution(solution)}")
 
 
-final_time = datetime.datetime.now()
-print(final_time-init_time)"""
+def adaptive_sa():
+    solution = get_sa_solution_adaptive_with_stages(5000)
+    print(f"Adapative SA: {evaluate_solution(solution)}")
 
-solution = get_sa_solution(100000)
-print(is_legal(solution))
-print(solution)
+
+for i in range(2):
+    p1 = Process(target=normal_sa)
+    p1.start()
+
+for i in range(2):
+    p2 = Process(target=adaptive_sa)
+    p2.start()
+
